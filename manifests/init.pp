afc6dcb404002fa3901bc435e0197e49459db56d
Thomas Linkin <trlinkin@gmail.com>
2014-07-29 15:47:43 -0400

    Correct syntax error in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
-class mcollective_plugin inherits mcollective_plugin::params(
+class mcollective_plugin(
   $mco_service = $::mcollective_plugin::params::service,
   $restart_mco = true,
-){
+)inherits mcollective_plugin::params {
   #haha, my hacky shit -- RAWR Collectars
   if $restart_mco {
     Mcollective_plugin::Plugin<||> {
